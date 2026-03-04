fc6e8ef6ca7080bd62e3c5782abe03a77d48e971
Sveta616 <svetamarinina44@gmail.com>
2026-03-04 15:22:08 +0500

    AddGeometryShape Library

---
+++ Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+    
+
+     public double Area ()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter ()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public string ToString ()
+        {
+            return $"Прямоугольник: Ширина = {Width}, Высота = {Height}, Площадь = {Area()}, Периметр = {Perimeter()}";
+        }
+
+
+
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GeometryShape
+{
+    public class Triangle
+    {
+        public double SideA {  get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public double Perimeter ()
+        {
+            return SideA + SideB + SideC;
+        }
+        public double Area ()
+        {
+            return Math.Sqrt((Perimeter() / 2) * ((Perimeter() / 2) - SideA) * ((Perimeter() / 2) - SideB) * ((Perimeter() / 2) - SideC));
+        }
+        public string ToString ()
+        {
+            return $"Треугольник: Первая сторона = {SideA}, Вторая сторона = {SideB}, Третья сторона = {SideC},\n Периметр = {Perimeter()} , Площадь = {Area()}";
+        }
+       
+        public string CheckingTheCorrectnes()
+        {
+            if(SideA+SideB<=SideC)
+            {
+                return "Такой треугольник не может существовать";
+            }
+            if (SideA + SideC <= SideB)
+            {
+                return "Такой треугольник не может существовать";
+            }
+            if (SideC + SideB <= SideB)
+            {
+                return "Такой треугольник не может существовать";
+            }
+            return null;
+        }
+
+    }
+}
