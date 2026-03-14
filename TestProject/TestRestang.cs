0423373d56cd165872c3ad365c02727bcf0e6534
kristinayungman <alyasezer778@gmail.com>
2026-03-14 17:30:21 +0500

    string

--- TestProject/TestRestang.cs
+++ TestProject/TestRestang.cs
@@ -32,11 +32,10 @@ namespace TestRectangle
             Rectangle rectangle = new Rectangle(6, 7);
             //string expected = "Прямоугольник: Ширина = 6, Высота = 7, Площадь = 42, Периметр = 26";
             string result = rectangle.ToString();
-            Assert.Contains("= 6,", result);
-            Assert.Contains("= 7,", result);
-            Assert.Contains("= 42,", result);
-            Assert.Contains("= 26", result);
-            Assert.StartsWith("Прямоугольник:", result);
+            Assert.Contains("\u0428\u0438\u0440\u0438\u043D\u0430 = 6", result); // Ширина = 6
+            Assert.Contains("\u0412\u044B\u0441\u043E\u0442\u0430 = 7", result); // Высота = 7
+            Assert.Contains("\u041F\u043B\u043E\u0449\u0430\u0434\u044C = 42", result); // Площадь = 42
+            Assert.Contains("\u041F\u0435\u0440\u0438\u043C\u0435\u0442\u0440 = 26", result); // Периметр = 26
         }
     }
 }
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51dddffd85c10dab3fb81c2509f31d6c10df8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533ad8beb7eefee96a320ffe73655086c3690e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
